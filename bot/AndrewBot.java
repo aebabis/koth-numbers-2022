8137ee1fbfc510fc690ceb0adebb6e59dc491125
Dearborn42 <amurf26@outlook.com>
2022-11-17 08:53:34 -0700

    Update AndrewBot.java

--- bot/AndrewBot.java
+++ bot/AndrewBot.java
@@ -5,7 +5,12 @@ import java.lang.Math;
 
 public class AndrewBot extends Bot {
     protected int _getMove(Game game) {
-		return (int) (Math.random()*10);
-        
+      if(LessGreedyBot.isBotAlive == true){
+        return ((int)Math.random()*10)+2;
+      }else if(GreedyBot.isBotAlive == true){
+        return ((int)Math.random()*10)+3;
+      }else{
+        return ((int)Math.random()*10)
+      }     
     }
 }
