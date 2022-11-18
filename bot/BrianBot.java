3770e8dc66e8bcfea88ff40a57aeb5c569052467
Brian <tsanbrian0@gmail.com>
2022-11-18 08:09:07 -0700

    edited BrianBot

--- bot/BrianBot.java
+++ bot/BrianBot.java
@@ -5,6 +5,11 @@ import runner.*;
 public class BrianBot extends Bot {
     protected int _getMove(Game game) {
         int theNumber = (int) (Math.random() * game.getNumAlive()) - 1;
+
+        if (theNumber == 0){
+            theNumber = (int) (Math.random() * game.getNumAlive()) - 1;
+        }
+
         if (theNumber <= 2){
             theNumber += 1;
         } else if (theNumber >= 10){
