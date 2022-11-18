a5854699cbc98ea09b8dd65faeaae4c19f53e04c
Brian <tsanbrian0@gmail.com>
2022-11-18 08:02:42 -0700

    Added BrianBot

---
+++ bot/BrianBot.java
@@ -0,0 +1,15 @@
+package bot;
+
+import runner.*;
+
+public class BrianBot extends Bot {
+    protected int _getMove(Game game) {
+        int theNumber = (int) (Math.random() * game.getNumAlive()) - 1;
+        if (theNumber <= 2){
+            theNumber += 1;
+        } else if (theNumber >= 10){
+            theNumber -= 2;
+        }
+        return theNumber;
+    }
+}
