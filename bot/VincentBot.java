01846e9842f4d5b514a306aa59e3b014b8a66d98
teranull <118197765+teranull@users.noreply.github.com>
2022-11-21 08:51:33 -0700

    Update VincentBot.java

--- bot/VincentBot.java
+++ bot/VincentBot.java
@@ -4,12 +4,16 @@ import runner.*;
 
 public class VincentBot extends Bot{
     protected int _getMove(Game game) {
-    	int dif = game.getNumBots() - game.getNumAlive();
-    	if(dif >0 && dif <=5) {
-    		dif= dif *2 + 1;
+    	int alive = game.getNumAlive();
+    	double rndm = Math.random()  +.1;
+//    	System.out.println("alive = " + alive + "\nrndom = " + rndm);
+    	if(alive <= 1) {
+    		alive = (int)  (rndm * 2 + 2) ^2;
+    	} else if (alive <10) {
+    		alive = (int) (alive * rndm + alive);
     	} else {
-    		dif =3;
+    		alive = (int) (Math.sqrt(rndm * alive * alive));
     	}
-    	return (int) (dif + dif * 0.5 * game.getNumRounds());
+    	return alive++;
     }
 }
