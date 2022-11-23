5baf4cd38fbd894f026afe7162de96a86e6f382e
Sabien Breshanaj <820663852@student.guhsdaz.org>
2022-11-23 09:47:38 -0700

    Made better

--- bot/TensBot.java
+++ bot/TensBot.java
@@ -3,6 +3,22 @@ import runner.*;
 
 public class TensBot extends Bot {
 	protected int _getMove(Game game) {
-		return (int) Math.round(2*(Math.ceil(Math.random()*10)));
+		int num = (int)(2*(Math.ceil(Math.random()*10)));
+		if (num > game.getNumAlive() && game.getNumAlive() > 4) {
+			if (num > 10) {
+				if (((num / 2) % 2) != 0) {
+					num = (num / 2) - 1;
+				} else {
+					num = (num / 2);
+				}
+			}
+		} else {
+			return game.getNumAlive();
+		}
+		if (Math.abs(num - game.getNumAlive()) != 0) {
+		return (int) num;
+		} else {
+			return 2;
+		}
 	}
-}
+} 
